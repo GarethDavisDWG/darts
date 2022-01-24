de3c9e7ff378d9b5cf4d1e0db36487c7430c1443
Gareth Davis <gdavis@softwareinstitute.com>
2022-01-24 15:00:45 +0000

    Added build file and POM properties for sonarcloud

--- src/main/java/com/tsi/davis/gareth/darts/Main.java
+++ src/main/java/com/tsi/davis/gareth/darts/Main.java
@@ -3,7 +3,7 @@ package com.tsi.davis.gareth.darts;
 
 public class Main {
 
-    public static void main(String args[]){
+    public static void main(String[] args){
         Dart garethDart = new Dart();
         System.out.println(garethDart);
 
